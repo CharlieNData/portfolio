9cf320b88608986b23db0512db8f1b90a6f4a488
CharlieNData <charlesejewers@gmail.com>
2021-05-04 15:31:42 +0100

    added experience card text and animation

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ function App() {
     <div className="App">
       <section className="front-page">
         <Header/>
-        <div className="main-section splash">
-          <h1>
-            Charlie<br></br>Jewers
-          </h1>
-          <h3>
-            Web developer,<br></br>graphic designer.
-          </h3>
+        <div className="main-section">
+          <div className="splash">
+            <h1>
+              Charlie<br></br>Jewers
+            </h1>
+            <h3>
+              Web developer,<br></br>graphic designer.
+            </h3>
+          </div>
+          <div className="react-info-text">
+                <p>
+                  I developed this website from scratch using HTML, CSS and JavaScript within the the <span style={{color: "red"}}>React.js</span> framework.
+                </p>
+          </div>
         </div>
+        
         <HomepageButton text="About Me" target="#about"/>
       </section>
       <section className="about" id="about">
@@ -44,14 +52,18 @@ function App() {
                 <ExperienceCard 
                   titleText="Software Engineer, Netcompany"
                   imgSrc='./images/wrench-solid.svg'
+                  description="Web design and development on a number of high-profile projects, such as the Ministry of Defence and NHS."
+                  skills={["React.js", "GitHub", "Jira"]}
                 />
                 <ExperienceCard 
                   titleText="Web Developer, IDS Ltd"
                   imgSrc='./images/laptop-code-solid.svg'
+                  skills={[]}
                   />
                 <ExperienceCard 
                   titleText="MComp Computer Science, University of Sheffield"
                   imgSrc='./images/university-solid.svg'
+                  skills={[]}
                 />
             </div>
           </div>    
--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react'
 
 export default class ExperienceCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hideCover: false
+        };
+
+        this.handleHideCover = this.handleHideCover.bind(this);
+    }
+
+    handleHideCover() {
+        this.setState({
+            hideCover: !this.state.hideCover
+        });
+    }
+
     render() {
 
         let titleText = this.props.titleText;
         let imgSrc = this.props.imgSrc;
+        let description = this.props.description;
+        let skills = this.props.skills;
+        let skillsList = skills.map((skill) => 
+        <li>
+            <p>{skill}</p>
+        </li>);
 
         return (
             <div className="experience-card">
-                <div className="experience-card-img">
-                    <img src={imgSrc}></img>
+                <div className="experience-card-content" onClick={this.handleHideCover}>
+                    <div className="experience-card-content-text">
+                        <p>{description}</p>
+                        <ul className="experience-card-content-skills">
+                            <p>Key Skills:</p>
+                            {skillsList}
+                        </ul>
+                    </div>
                 </div>
-                <div className="experience-card-text">
-                   <p>{titleText}</p>
+                <div className={`experience-card-cover ${this.state.hideCover ? 'cover-hidden' : ''}`} onClick={this.handleHideCover}>
+                    <div className="experience-card-img">
+                        <img src={imgSrc}></img>
+                    </div>
+                    <div className="experience-card-text">
+                    <p>{titleText}</p>
+                    </div>
                 </div>
             </div>
+            
         )
     }
 }
